ce44503b789bd3fe8599d62829313f5010406f13
aliosmankaya <aokaya16@gmail.com>
2023-12-18 01:40:43 +0300

    fix: update inference

--- inference.py
+++ inference.py
@@ -4,4 +4,4 @@ from data import decode
 from parameters import device
 
 context = torch.zeros((1, 1), dtype=torch.long, device=device)
-print(decode)
+print(decode(m.generate(context, max_new_tokens=500)[0].tolist()))
