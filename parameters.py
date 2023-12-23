52d745e456c8e3e68ed1095b68f4782fe00a91a1
aliosmankaya <aokaya16@gmail.com>
2023-12-23 19:41:14 +0300

    feat: update network architecture

--- parameters.py
+++ parameters.py
@@ -1,15 +1,30 @@
 import torch
 
+eval_interval = 250
+eval_iters = 200
+log_interval = 10
+gradient_accumulation_steps = 1
 batch_size = 64
 block_size = 256
-max_iters = 5000
-eval_interval = 500
-learning_rate = 3e-4
-eval_iters = 200
-n_embed = 384
-num_heads = 6
 n_layer = 6
+n_head = 6
+n_embd = 384
 dropout = 0.2
-device = "cuda" if torch.cuda.is_available() else "cpu"
+learning_rate = 1e-3
+max_iters = 5000
+lr_decay_iters = 5000
+min_lr = 1e-4
+beta1 = 0.9
+beta2 = 0.99
+warmup_iters = 100
+weight_decay = 1e-1
+bias = False
+dtype = (
+    "bfloat16"
+    if torch.cuda.is_available() and torch.cuda.is_bf16_supported()
+    else "float16"
+)
+device = "cpu"
+device_type = "cpu"
 is_save = False
 path = "./model.pt"
--- train.py
+++ train.py
@@ -1,7 +1,9 @@
+import math
+
 import torch
 
-from data import get_batch
-from network import GPTLanguageModel
+from data import get_batch, vocab_size
+from network import GPT, GPTConfig
 from parameters import *
 
 torch.manual_seed(1337)
@@ -22,11 +24,38 @@ def estimate_loss():
     return out
 
 
-model = GPTLanguageModel()
+def get_lr(it):
+    if it < warmup_iters:
+        return learning_rate * it / warmup_iters
+    if it > lr_decay_iters:
+        return min_lr
+    decay_ratio = (it - warmup_iters) / (lr_decay_iters - warmup_iters)
+    assert 0 <= decay_ratio <= 1
+    coeff = 0.5 * (1.0 + math.cos(math.pi * decay_ratio))
+    return min_lr + coeff * (learning_rate - min_lr)
+
+
+model_args = dict(
+    n_layer=n_layer,
+    n_head=n_head,
+    n_embd=n_embd,
+    block_size=block_size,
+    bias=bias,
+    vocab_size=vocab_size,
+    dropout=dropout,
+)
+
+gptconf = GPTConfig(**model_args)
+model = GPT(gptconf)
+if block_size < model.config.block_size:
+    model.crop_block_size(block_size)
+    model_args["block_size"] = block_size
 m = model.to(device)
 print(sum(p.numel() for p in m.parameters()) / 1e6, "M parameters")
 
-optimizer = torch.optim.AdamW(model.parameters(), lr=learning_rate)
+optimizer = model.configure_optimizers(
+    weight_decay, learning_rate, (beta1, beta2), device_type
+)
 
 for iter in range(max_iters):
     if iter % eval_interval == 0 or iter == max_iters - 1:
