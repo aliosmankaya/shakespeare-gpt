e7a8aebf4ba45e543559545e35a9cc2490bd8e89
aliosmankaya <aokaya16@gmail.com>
2023-12-23 22:45:14 +0300

    fix: update inference

--- inference.py
+++ inference.py
@@ -1,13 +1,27 @@
 import torch
 
-from data import decode
-from network import GPTLanguageModel
-from parameters import device, path
+from data import decode, vocab_size
+from network import GPT, GPTConfig
+from parameters import *
 
-m = GPTLanguageModel()
+model_args = dict(
+    n_layer=n_layer,
+    n_head=n_head,
+    n_embd=n_embd,
+    block_size=block_size,
+    bias=bias,
+    vocab_size=vocab_size,
+    dropout=dropout,
+)
+
+gptconf = GPTConfig(**model_args)
+m = GPT(gptconf)
 m.to(device)
 m.load_state_dict(torch.load(path))
 m.eval()
 
 context = torch.zeros((1, 1), dtype=torch.long, device=device)
-print(decode(m.generate(context, max_new_tokens=500)[0].tolist()))
+
+with open("gen.txt", "w") as f:
+    text = decode(m.generate(context, max_new_tokens=5000)[0].tolist())
+    f.write(text)
